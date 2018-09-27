a04a7367650235e2a79d25f2af46c24ff82100fb
Vachet Virginie <vachetvirginie@gmail.com>
2018-09-27 16:58:06 +0200

    test react native expo

---
+++ utils/WeatherAPIKey.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'da1dd050d4490c914427c2ee94907caf';
---
+++ utils/WeatherConditions.js
@@ -0,0 +1,51 @@
+export const weatherConditions = {
+  Rain: {
+    color: '#005BEA',
+    title: 'Raining',
+    subtitle: 'Get a cup of coffee',
+    icon: 'weather-rainy'
+  },
+  Clear: {
+    color: '#f7b733',
+    title: 'Soleil 🕶️',
+    subtitle: 'Ca pique les yeux',
+    icon: 'weather-sunny'
+  },
+  Thunderstorm: {
+    color: '#616161',
+    title: 'Orage  ⛈️',
+    subtitle: 'Oh pinaise !',
+    icon: 'weather-lightning'
+  },
+  Clouds: {
+    color: '#1F1C2C',
+    title: 'Clouds ☁️',
+    subtitle: 'Everywhere ',
+    icon: 'weather-cloudy'
+  },
+
+  Snow: {
+    color: '#00d2ff',
+    title: 'Neige ☃️',
+    subtitle: 'Winter is coming!!',
+    icon: 'weather-snowy'
+  },
+  Drizzle: {
+    color: '#076585',
+    title: 'Bruine  ☔',
+    subtitle: 'ça fait juste galerer',
+    icon: 'weather-hail'
+  },
+  Haze: {
+    color: '#66A6FF',
+    title: 'Brume 🌁',
+    subtitle: 'On se croirai à Londres ',
+    icon: 'weather-hail'
+  },
+  Mist: {
+    color: '#3CD3AD',
+    title: 'Brouillard 🌫️',
+    subtitle: "Bon chance️ !",
+    icon: 'weather-fog'
+  }
+};
